55f14e2312810b8023018ff10394dcc730032652
jdgs-qa <jonathan.squire@academytrainee.com>
2019-07-19 11:22:54 +0100

    Added rest classes
    Catalogue Controller, including searching for items by a parameter
    -- Useful for implementing in the custom search feature
    Account Controller, including searching for accounts by a parameter
    -- Used to verify account details - no list of users will be public

---
+++ penHeavenAPI/src/main/java/com/qa/penHeavenAPI/rest/CatalogueController.java
@@ -0,0 +1,72 @@
+package com.qa.penHeavenAPI.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/catalogue")
+public class CatalogueController {
+
+	@Inject
+	private CatalogueService catalogueService;
+	
+	@GET
+	@Path("/getAll")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAllItems() {
+		return this.catalogueService.getAllItems();
+	}
+	
+	@POST
+	@Path("/create")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String createItem(String item) {
+		return this.catalogueService.createItem(item);
+	}
+	
+	@DELETE
+	@Path("/delete")
+	public String deleteItem(String itemId) {
+		return this.catalogueService.deleteItem(itemId);
+	}
+	
+	@POST
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String updateItem(String itemId, String item) {
+		return this.catalogueService.updateItem(itemId, item);
+	}
+	
+	@GET
+	@Path("/searchName")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getItemByName(String itemName) {
+		return this.catalogueService.getItemByName(itemName);
+	}
+	
+	@GET
+	@Path("/searchBrand")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getItemByBrand(String itemBrand) {
+		return this.catalogueService.getItemByBrand(itemBrand);
+	}
+	
+	@GET
+	@Path("/searchColour")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getItemByColour(String itemColour) {
+		return this.catalogueService.getItemByColour(itemColour);
+	}
+	
+	@GET
+	@Path("/searchType")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getItemByType(String itemType) {
+		return this.catalogueService.getItemByType(itemType);
+	}
+}
