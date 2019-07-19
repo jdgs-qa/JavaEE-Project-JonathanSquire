eb088e943364e1c171f519cb4eba215a259e13b9
jdgs-qa <jonathan.squire@academytrainee.com>
2019-07-19 10:29:58 +0100

    New Exceptions Created - currently empty

---
+++ penHeavenAPI/src/main/java/com/qa/penHeavenAPI/exceptions/ItemNotFoundExcpetion.java
@@ -0,0 +1,8 @@
+package com.qa.penHeavenAPI.exceptions;
+
+public class ItemNotFoundExcpetion extends RuntimeException{
+
+	private static final long serialVersionUID = 2657498265981536006L;
+	
+
+}
