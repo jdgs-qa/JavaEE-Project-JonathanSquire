3839454ece55e7853d624bfdb0084d235090734f
jdgs-qa <jonathan.squire@academytrainee.com>
2019-07-19 11:46:36 +0100

    Updated Repo to have string constants for status

--- penHeavenAPI/src/main/java/com/qa/penHeavenAPI/persistence/repo/AccountRepo.java
+++ penHeavenAPI/src/main/java/com/qa/penHeavenAPI/persistence/repo/AccountRepo.java
@@ -1,6 +1,13 @@
 package com.qa.penHeavenAPI.persistence.repo;
 
 public interface AccountRepo {
+	
+	public static final String SUCCESS_ADD = "Successfully created account";
+	public static final String FAIL_ADD = "Failed to create account";
+	public static final String SUCCESS_DEL = "Successfully deleted account";
+	public static final String FAIL_DEL = "Failed to delete account";
+	public static final String SUCCESS_UPDATE = "Successfully updated account details";
+	public static final String FAIL_UPDATE = "Failed to update account details";
 
 	String getAllAccounts();
 	
--- penHeavenAPI/src/main/java/com/qa/penHeavenAPI/persistence/repo/CatalogueItemRepo.java
+++ penHeavenAPI/src/main/java/com/qa/penHeavenAPI/persistence/repo/CatalogueItemRepo.java
@@ -1,6 +1,14 @@
 package com.qa.penHeavenAPI.persistence.repo;
 
 public interface CatalogueItemRepo {
+	
+	public static final String SUCCESS_ADD = "Successfully added item to catalogue";
+	public static final String FAIL_ADD = "Failed to add item to catalogue";
+	public static final String SUCCESS_DEL = "Successfully deleted item from catalogue";
+	public static final String FAIL_DEL = "Failed to delete item from catalogue";
+	public static final String SUCCESS_UPDATE = "Successfully updated item details";
+	public static final String FAIL_UPDATE = "Failed to update item details";
+	
 
 	String getAllItems();
 	
