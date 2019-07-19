2d39e42eedad0d3b36f69979165e3891ea6fb773
jdgs-qa <jonathan.squire@academytrainee.com>
2019-07-19 12:42:00 +0100

    Adde to CatalogueDBRepo

--- penHeavenAPI/src/main/java/com/qa/penHeavenAPI/persistence/repo/CatalogueDBRepo.java
+++ penHeavenAPI/src/main/java/com/qa/penHeavenAPI/persistence/repo/CatalogueDBRepo.java
@@ -7,38 +7,55 @@ import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
+import com.qa.penHeavenAPI.exceptions.ItemNotFoundExcpetion;
+import com.qa.penHeavenAPI.persistence.domain.CatalogueItem;
 import com.qa.penHeavenAPI.util.JSONUtil;
 
 @Default
 @Transactional(value = TxType.SUPPORTS)
 public class CatalogueDBRepo implements CatalogueItemRepo {
-	
+
 	@Inject
 	JSONUtil j = new JSONUtil();
-	
+
 	@PersistenceContext(unitName = "primary")
 	private EntityManager itemEntityManager;
 
 	public String getAllItems() {
-		// TODO Auto-generated method stub
-		return null;
+		return j.getJSONforObject(this.itemEntityManager.createQuery("Select i FROM Item i", CatalogueItem.class));
 	}
 
 	public String createItem(String item) {
-		// TODO Auto-generated method stub
-		return null;
+		this.itemEntityManager.persist(j.getObjectForJSON(item, CatalogueItem.class));
+		return SUCCESS_ADD;
 	}
 
 	@Transactional(value = TxType.REQUIRED)
 	public String deleteItem(String itemId) {
-		// TODO Auto-generated method stub
-		return null;
+		CatalogueItem i = this.itemEntityManager.find(CatalogueItem.class, itemId);
+		if (i == null) {
+			throw new ItemNotFoundExcpetion();
+		} else {
+			this.itemEntityManager.remove(i);
+			return SUCCESS_DEL;
+		}
+
 	}
 
 	@Transactional(value = TxType.REQUIRED)
 	public String updateItem(String itemId, String item) {
-		// TODO Auto-generated method stub
-		return null;
+		CatalogueItem iNew = j.getObjectForJSON(item, CatalogueItem.class);
+		CatalogueItem iOld = this.itemEntityManager.find(CatalogueItem.class, itemId);
+		if (iOld == null) {
+			throw new ItemNotFoundExcpetion();
+		} else {
+			iOld.setItemBrand(iNew.getItemBrand());
+			iOld.setItemColour(iNew.getItemColour());
+			iOld.setItemName(iNew.getItemName());
+			iOld.setItemType(iNew.getItemType());
+			this.itemEntityManager.persist(iOld);
+			return SUCCESS_UPDATE;
+		}
 	}
 
 	public String getItemByName(String itemName) {
